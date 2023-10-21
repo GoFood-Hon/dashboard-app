830467fd84664020b0b50216d9706a1b4d367564
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-10-21 13:59:54 -0600

    style: added transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Button from "./Button"
 import { restaurantList } from "../utils/restaurants"
 import { Icon } from "./Icon"
 import { useSelector } from "react-redux"
 import { NavLink, useLocation, useNavigate } from "react-router-dom"
-import { Popover, useMantineColorScheme, Button as MantineButton } from "@mantine/core"
+import { Popover, Transition } from "@mantine/core"
 
 export default function Header() {
   const user = useSelector((state) => state.user.value)
   const location = useLocation()
   const navigate = useNavigate()
+
+  const [opened, setOpened] = useState(false)
 
   useEffect(() => {
     if (location.pathname === "/logout") {
@@ -39,7 +41,15 @@ export default function Header() {
           <div className="font-semibold">{user.name}</div>
           <div className="text-gray-400">{user.email}</div>
         </div>
-        <Popover width={250} withArrow shadow="md" position="bottom-end" offset={{ mainAxis: 10, crossAxis: -10 }} arrowSize={12}>
+        <Popover
+          width={250}
+          withArrow
+          shadow="md"
+          position="bottom-end"
+          offset={{ mainAxis: 10, crossAxis: -10 }}
+          arrowSize={12}
+          opened={opened}
+          onChange={setOpened}>
           <Popover.Target>
             <button aria-label="Update dimensions">
               <img
@@ -48,33 +58,38 @@ export default function Header() {
                 alt="avatar"
                 width={40}
                 height={40}
+                onClick={() => setOpened((o) => !o)}
               />
             </button>
           </Popover.Target>
           <Popover.Dropdown>
-            <div className="p-3">
-              <div className="flex flex-col gap-2.5 justify-center items-center">
-                <img
-                  className="rounded-full hover:outline hover:outline-cyan-500 hover:duration-75 hover:ease-in-out hover:outline-offset-2"
-                  src="https://api.dicebear.com/7.x/micah/svg?backgroundColor=b6e3f4"
-                  alt="avatar"
-                  width={70}
-                  height={70}
-                />
-              </div>
-              <div className="flex flex-col items-center">
-                <div className="font-semibold">M Geovany</div>
-                <div className="text-gray-400 text-sm">marlongeo1999@gmail.com</div>
-              </div>
-              <Button text={"Manejar cuenta"} className={"mt-5 border border-slate-400 rounded text-sm"} />
-              <NavLink
-                className={
-                  "mt-3 dark:border-slate-100 border-slate-400 rounded text-sm bg-slate-700 text-white flex h-10 w-full items-center justify-center space-x-3 shadow-sm transition-all duration-700 focus:outline-none mb-3"
-                }
-                to="/logout">
-                <span>Cerrar sesión</span>
-              </NavLink>
-            </div>
+            <Transition mounted={opened} transition="slide-left" duration={400} timingFunction="ease-in-out">
+              {(styles) => (
+                <div className="p-3" style={styles}>
+                  <div className="flex flex-col gap-2.5 justify-center items-center">
+                    <img
+                      className="rounded-full hover:outline hover:outline-cyan-500 hover:duration-75 hover:ease-in-out hover:outline-offset-2"
+                      src="https://api.dicebear.com/7.x/micah/svg?backgroundColor=b6e3f4"
+                      alt="avatar"
+                      width={70}
+                      height={70}
+                    />
+                  </div>
+                  <div className="flex flex-col items-center">
+                    <div className="font-semibold">M Geovany</div>
+                    <div className="text-gray-400 text-sm">marlongeo1999@gmail.com</div>
+                  </div>
+                  <Button text={"Manejar cuenta"} className={"mt-5 border border-slate-400 rounded text-sm"} />
+                  <NavLink
+                    className={
+                      "mt-3 dark:border-slate-100 border-slate-400 rounded text-sm bg-slate-700 text-white flex h-10 w-full items-center justify-center space-x-3 shadow-sm transition-all duration-700 focus:outline-none mb-3"
+                    }
+                    to="/logout">
+                    <span>Cerrar sesión</span>
+                  </NavLink>
+                </div>
+              )}
+            </Transition>
           </Popover.Dropdown>
         </Popover>
       </div>
