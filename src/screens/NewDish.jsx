f4734650404b8d098fdf58cab2068db478c01578
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-10-23 15:12:00 -0600

    feat: added dropzone

--- src/screens/NewDish.jsx
+++ src/screens/NewDish.jsx
@@ -20,12 +20,69 @@ export default function NewDish() {
     </Link>
   ))
 
+  const GeneralInformation = () => {
+    return (
+      <Grid>
+        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+          <div className="w-full h-full items-center justify-center flex bg-white rounded-2xl border border-blue-100 p-4">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full">
+              <InputField
+                label="Nombre (Obligatorio)"
+                name="dishName"
+                register={register}
+                rules={inputRequired}
+                errors={errors}
+                placeholder="Ej. Pollo con papas"
+                className="text-black"
+              />
+              <InputTextAreaField
+                label="Descripción (Obligatorio)"
+                name="dishDescription"
+                rules={inputRequired}
+                register={register}
+                errors={errors}
+                placeholder="Ej. Rico pollo con papas, salsas..."
+              />
+            </form>
+          </div>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+          <div className="w-full h-full bg-white rounded-2xl border border-blue-100 p-4">
+            <Dropzone
+              onDrop={(files) => console.log("accepted files", files)}
+              onReject={(files) => console.log("rejected files", files)}
+              accept={IMAGE_MIME_TYPE}>
+              <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: "none" }}>
+                <Dropzone.Accept>
+                  <IconUpload style={{ width: rem(52), height: rem(52), color: "var(--mantine-color-blue-6)" }} stroke={1.5} />
+                </Dropzone.Accept>
+                <Dropzone.Reject>
+                  <IconX style={{ width: rem(52), height: rem(52), color: "var(--mantine-color-red-6)" }} stroke={1.5} />
+                </Dropzone.Reject>
+                <Dropzone.Idle>
+                  <IconPhoto style={{ width: rem(52), height: rem(52), color: "var(--mantine-color-dimmed)" }} stroke={1.5} />
+                </Dropzone.Idle>
+
+                <div>
+                  <Text size="xl" inline>
+                    Seleccione una imagen destacada
+                  </Text>
+                  <Text size="sm" c="dimmed" inline mt={7}>
+                    Haga click o arrastre una imagen que sera usada junto con el platillo
+                  </Text>
+                </div>
+              </Group>
+            </Dropzone>
+          </div>
+        </Grid.Col>
+      </Grid>
+    )
+  }
   const groceries = [
     {
       title: "Información general",
       requirement: "Obligatorio",
-      description:
-        "Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads."
+      description: <GeneralInformation />
     },
     {
       title: "Complementos",
@@ -72,61 +129,7 @@ export default function NewDish() {
           <span className="text-sky-950 text-base font-normal ml-1">({item?.requirement})</span>
         </div>
       </Accordion.Control>
-      <Accordion.Panel>
-        <Grid>
-          <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-            <div className="w-full h-full items-center justify-center flex bg-white rounded-2xl border border-blue-100 p-4">
-              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full">
-                <InputField
-                  label="Nombre (Obligatorio)"
-                  name="dishName"
-                  register={register}
-                  rules={inputRequired}
-                  errors={errors}
-                  placeholder="Ej. Pollo con papas"
-                />
-                <InputTextAreaField
-                  label="Descripción (Obligatorio)"
-                  name="dishDescription"
-                  rules={inputRequired}
-                  register={register}
-                  errors={errors}
-                  placeholder="Ej. Rico pollo con papas, salsas..."
-                />
-              </form>
-            </div>
-          </Grid.Col>
-          <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-            <div className="w-full h-full bg-white rounded-2xl border border-blue-100 p-4">
-              <Dropzone
-                onDrop={(files) => console.log("accepted files", files)}
-                onReject={(files) => console.log("rejected files", files)}
-                accept={IMAGE_MIME_TYPE}>
-                <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: "none" }}>
-                  <Dropzone.Accept>
-                    <IconUpload style={{ width: rem(52), height: rem(52), color: "var(--mantine-color-blue-6)" }} stroke={1.5} />
-                  </Dropzone.Accept>
-                  <Dropzone.Reject>
-                    <IconX style={{ width: rem(52), height: rem(52), color: "var(--mantine-color-red-6)" }} stroke={1.5} />
-                  </Dropzone.Reject>
-                  <Dropzone.Idle>
-                    <IconPhoto style={{ width: rem(52), height: rem(52), color: "var(--mantine-color-dimmed)" }} stroke={1.5} />
-                  </Dropzone.Idle>
-
-                  <div>
-                    <Text size="xl" inline>
-                      Seleccione una imagen destacada
-                    </Text>
-                    <Text size="sm" c="dimmed" inline mt={7}>
-                      Haga click o arrastre una imagen que sera usada junto con el platillo
-                    </Text>
-                  </div>
-                </Group>
-              </Dropzone>
-            </div>
-          </Grid.Col>
-        </Grid>
-      </Accordion.Panel>
+      <Accordion.Panel>{item.description}</Accordion.Panel>
     </Accordion.Item>
   ))
 
