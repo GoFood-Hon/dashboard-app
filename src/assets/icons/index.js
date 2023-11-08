09b6668cfc29b04c0708404910d5e6fbbba46194
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-11-08 13:08:32 -0600

    refactor: exported icon files

---
+++ src/assets/icons/index.js
@@ -0,0 +1,41 @@
+export * from "./AlarmIcon"
+
+export * from "./DashboardIcon"
+
+export * from "./ChevronUp"
+
+export * from "./ShoppingIcon"
+
+export * from "./InvoiceIcon"
+
+export * from "./MenuIcon"
+
+export * from "./LayoutIcon"
+
+export * from "./BranchIcon"
+
+export * from "./UserIcon"
+
+export * from "./ExitIcon"
+
+export * from "./OneTouchIcon"
+
+export * from "./MoneyIcon"
+
+export * from "./DownIcon"
+
+export * from "./UpIcon"
+
+export * from "./BagIcon"
+
+export * from "./WarehouseIcon"
+
+export * from "./TimerIcon"
+
+export * from "./CalendarIcon"
+
+export * from "./MoreIcon"
+
+export * from "./ChevronDown"
+
+export * from "./CloseIcon"
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,25 +1,27 @@
 import React from "react"
-import { ReactSVG } from "react-svg"
-import { CloseIcon } from "../assets/icons/CloseIcon"
-import { DashboardIcon } from "../assets/icons/DashboardIcon"
-import { ShoppingIcon } from "../assets/icons/ShoppingIcon"
-import { InvoiceIcon } from "../assets/icons/InvoiceIcon"
-import { MenuIcon } from "../assets/icons/MenuIcon"
-import { LayoutIcon } from "../assets/icons/LayoutIcon"
-import { BranchIcon } from "../assets/icons/BranchIcon"
-import { UserIcon } from "../assets/icons/UserIcon"
-import { ExitIcon } from "../assets/icons/ExitIcon"
-import { OneTouchIcon } from "../assets/icons/OneTouchIcon"
-import { MoneyIcon } from "../assets/icons/MoneyIcon"
-import { DownIcon } from "../assets/icons/DownIcon"
-import { UpIcon } from "../assets/icons/UpIcon"
-import { BagIcon } from "../assets/icons/BagIcon"
-import { WarehouseIcon } from "../assets/icons/WarehouseIcon"
-import { TimerIcon } from "../assets/icons/TimerIcon"
-import { CalendarIcon } from "../assets/icons/CalendarIcon"
-import { MoreIcon } from "../assets/icons/MoreIcon"
-import { ChevronDown } from "../assets/icons/ChevronDown"
-import { ChevronUp } from "../assets/icons/ChevronUp"
+
+import {
+  BagIcon,
+  BranchIcon,
+  CalendarIcon,
+  ChevronDown,
+  ChevronUp,
+  CloseIcon,
+  DashboardIcon,
+  DownIcon,
+  ExitIcon,
+  InvoiceIcon,
+  LayoutIcon,
+  MenuIcon,
+  MoneyIcon,
+  MoreIcon,
+  OneTouchIcon,
+  ShoppingIcon,
+  TimerIcon,
+  UpIcon,
+  UserIcon,
+  WarehouseIcon
+} from "../assets/icons"
 
 export const iconComponents = {
   close: <CloseIcon />,
