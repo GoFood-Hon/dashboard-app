6e2be1c1cc759a92ffd5b7510de2822845e08538
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-11-14 13:56:07 -0600

    refactor: sort popover

--- src/components/SortPopover.jsx
+++ src/components/SortPopover.jsx
@@ -5,7 +5,13 @@ import { colors } from "../theme/colors"
 import Button from "./Button"
 
 export default function SortPopover() {
-  const [checked, setChecked] = useState(false)
+  const [idChecked, setIdChecked] = useState(false)
+  const [userChecked, setUserChecked] = useState(false)
+  const [dateChecked, setDateChecked] = useState(false)
+
+  const handleApplyFilters = () => {}
+
+  const handleResetFilters = () => {}
 
   return (
     <Popover width={300} position="bottom" withArrow shadow="md" radius={20} offset={{ mainAxis: 2, crossAxis: -130 }}>
@@ -19,66 +25,72 @@ export default function SortPopover() {
         <section className="border-b-[0.50px] border-blue-100">
           <div className="flex flex-row justify-between p-4 text-sm py-2">
             <p>Id</p>
-            <p>Reiniciar</p>
+            <a className="cursor-pointer" onClick={() => setIdChecked(false)}>
+              Reiniciar
+            </a>
           </div>
           <div className="flex flex-col p-4 gap-2 pt-0">
             <Radio
               value="ASC"
               label="Ascendente"
               color={colors.primary_text}
-              checked={checked}
-              onChange={(event) => setChecked(event.currentTarget.checked)}
+              checked={idChecked}
+              onChange={(event) => setIdChecked(event.currentTarget.checked)}
             />
             <Radio
               value="DESC"
               label="Descendente"
               color={colors.primary_text}
-              checked={!checked}
-              onChange={(event) => setChecked(!event.currentTarget.checked)}
+              checked={!idChecked}
+              onChange={(event) => setIdChecked(!event.currentTarget.checked)}
             />
           </div>
         </section>
         <section className="border-b-[0.50px] border-blue-100">
           <div className="flex flex-row justify-between p-4 py-2 text-sm">
             <p>Usuario</p>
-            <p>Reiniciar</p>
+            <a className="cursor-pointer" onClick={() => setUserChecked(false)}>
+              Reiniciar
+            </a>
           </div>
           <div className="flex flex-col p-4 gap-2 pt-0">
             <Radio
               value="ASC"
               label="Ascendente"
               color={colors.primary_text}
-              checked={checked}
-              onChange={(event) => setChecked(event.currentTarget.checked)}
+              checked={userChecked}
+              onChange={(event) => setUserChecked(event.currentTarget.checked)}
             />
             <Radio
               value="DESC"
               label="Descendente"
               color={colors.primary_text}
-              checked={!checked}
-              onChange={(event) => setChecked(!event.currentTarget.checked)}
+              checked={!userChecked}
+              onChange={(event) => setUserChecked(!event.currentTarget.checked)}
             />
           </div>
         </section>
         <section className="border-b-[0.50px] border-blue-100">
           <div className="flex flex-row justify-between p-4 py-2 text-sm">
             <p>Fecha</p>
-            <p>Reiniciar</p>
+            <a className="cursor-pointer" onClick={() => setDateChecked(false)}>
+              Reiniciar
+            </a>
           </div>
           <div className="flex flex-col p-4 gap-2 pt-0">
             <Radio
               value="ASC"
               label="Ascendente"
               color={colors.primary_text}
-              checked={checked}
-              onChange={(event) => setChecked(event.currentTarget.checked)}
+              checked={dateChecked}
+              onChange={(event) => setDateChecked(event.currentTarget.checked)}
             />
             <Radio
               value="DESC"
               label="Descendente"
               color={colors.primary_text}
-              checked={!checked}
-              onChange={(event) => setChecked(!event.currentTarget.checked)}
+              checked={!dateChecked}
+              onChange={(event) => setDateChecked(!event.currentTarget.checked)}
             />
           </div>
         </section>
@@ -87,11 +99,13 @@ export default function SortPopover() {
             <Button
               text="Borrar Filtros"
               className="rounded border border-sky-950 justify-center items-center gap-2.5 inline-flex"
+              onClick={() => handleResetFilters()}
             />
             <Button
               text="Aplicar Filtros"
               className="bg-sky-950 rounded justify-center items-center gap-2.5 inline-flex"
               textClassName="text-white"
+              onClick={() => handleApplyFilters()}
             />
           </div>
         </section>
