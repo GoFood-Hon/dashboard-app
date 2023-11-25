185b14818207327b80fa366106a2227958a29662
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-11-25 14:34:41 -0600

    feat: created GeneralInformation for Menu Form

---
+++ src/screens/Menu/GeneralInformationForm.jsx
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from "react"
+import { CloseIcon, Grid, Group, Text, rem } from "@mantine/core"
+import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone"
+import { IconPhoto } from "@tabler/icons-react"
+import toast from "react-hot-toast"
+import InputField from "../../components/Form/InputField"
+import InputTextAreaField from "../../components/Form/InputTextAreaField"
+import InputSearchCombobox from "../../components/Form/InputSearchCombobox"
+import { colors } from "../../theme/colors"
+import { bytesToMB } from "../../utils"
+
+export default function GeneralInformationForm({ register, errors, setValue, isDataCleared }) {
+  const [images, setImages] = useState([])
+  const [fileInformation, setFileInformation] = useState(null)
+
+  const handleDrop = (acceptedFiles) => {
+    if (acceptedFiles.length > 0) {
+      const [file] = acceptedFiles
+      setFileInformation(file)
+      setImages(acceptedFiles)
+      setValue("files", acceptedFiles)
+      toast.success("Archivos aceptados 👍", { duration: 7000 })
+    }
+  }
+  const deleteImage = () => {
+    setFileInformation(null)
+    setImages([])
+  }
+
+  useEffect(() => {
+    if (isDataCleared) {
+      deleteImage()
+    }
+  }, [isDataCleared])
+
+  const previews = images.map((file, index) => {
+    const imageUrl = URL.createObjectURL(file)
+    return (
+      <img
+        key={index}
+        src={imageUrl}
+        onLoad={() => URL.revokeObjectURL(imageUrl)}
+        className="h-14 w-14 rounded-xl object-cover object-center border m-1"
+      />
+    )
+  })
+
+  const menuType = [
+    {
+      id: 1,
+      name: "Normal"
+    },
+    {
+      id: 2,
+      name: "Familiar"
+    }
+  ]
+
+  return (
+    <Grid>
+      <Grid.Col span={{ base: 12, md: 8, lg: 8 }}>
+        <div className="w-full h-full items-center justify-center flex bg-white rounded-2xl border border-blue-100 p-4">
+          <div className="flex flex-col w-full">
+            <InputField label="Nombre (Obligatorio)" name="name" register={register} errors={errors} className="text-black" />
+
+            <InputTextAreaField label="Descripción (Obligatorio)" name="description" register={register} errors={errors} />
+
+            <div className="mt-3">
+              <InputSearchCombobox
+                label="Tipo de menu"
+                name={"type"}
+                placeholder="Buscar Menus"
+                emptyMessage="Sin categorías  "
+                items={menuType}
+                register={register}
+                errors={errors}
+                setValue={setValue}
+                color={colors.primary_button}
+              />
+            </div>
+          </div>
+        </div>
+      </Grid.Col>
+      <Grid.Col span={{ base: 12, md: 4, lg: 4 }}>
+        <div className="flex flex-col justify-center items-center w-full h-full bg-white rounded-2xl border border-blue-100 p-4">
+          {previews.length > 0 ? (
+            <div className="w-full">
+              <Text size="lg" inline className="text-left mb-5">
+                Imagen seleccionada:
+              </Text>
+              <div className="flex flex-row justify-center items-center rounded-2xl w-full border border-slate-200 my-3">
+                <div className="flex flex-row w-full items-center gap-2 flex-wrap p-2">
+                  {previews}
+                  <div className="flex flex-col">
+                    <Text className="font-semibold italic">{fileInformation?.name}</Text>
+                    <Text className="font-semibold" size="sm" c="dimmed" inline>
+                      {bytesToMB(fileInformation?.size)} MB
+                    </Text>
+                  </div>
+                </div>
+                <button onClick={deleteImage} className="pr-3">
+                  <CloseIcon size={24} />
+                </button>
+              </div>
+            </div>
+          ) : (
+            <Dropzone onDrop={handleDrop} accept={IMAGE_MIME_TYPE}>
+              <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: "none" }}>
+                <div className="flex items-center flex-col">
+                  <IconPhoto style={{ width: rem(52), height: rem(52), color: "var(--mantine-color-dimmed)" }} stroke={1.5} />
+                  <Text size="xl" inline className="text-center">
+                    Seleccione una imagen destacada
+                  </Text>
+                  <Text size="sm" c="dimmed" inline mt={7} className="text-center leading-10">
+                    Haga click o arrastre una imagen que sera usada junto con el menu
+                  </Text>
+                </div>
+              </Group>
+            </Dropzone>
+          )}
+          {errors.files && <p className="text-red-500 text-center w-full">* Imagen es requerida.</p>}
+        </div>
+      </Grid.Col>
+    </Grid>
+  )
+}
