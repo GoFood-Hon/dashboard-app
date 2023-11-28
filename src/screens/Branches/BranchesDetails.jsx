f1b1ea5d8e9558010607f95fb6a80735b3eb01f9
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-11-27 22:02:08 -0600

    feat: branches details

---
+++ src/screens/Branches/BranchesDetails.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import BaseLayout from "../../components/BaseLayout"
+
+export default function BranchesDetails() {
+  return (
+    <BaseLayout>
+      <h1>Details</h1>
+    </BaseLayout>
+  )
+}
