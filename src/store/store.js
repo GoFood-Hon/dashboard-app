44ae4c060c87dbfa83d8c86d23e4d4aa904bd20f
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-12-20 12:08:56 -0600

    fix: corrected spelling

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "./features/userSlice"
 import restaurantReducer from "./features/restaurantSlice"
-import dishesReducer from "./features/DishesSlice"
+import dishesReducer from "./features/dishesSlice"
 import complementsReducer from "./features/complementsSlice"
 import dishesCategoriesReducer from "./features/categorySlice"
 import menuReducer from "./features/menuSlice"
