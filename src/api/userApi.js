9d4d2e3837874431a288023eff924ef802137473
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-12-26 21:20:20 -0600

    feat: users got from the api to the table

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,7 +1,28 @@
 import axiosClient from "./axiosClient"
 
 const userApi = {
-  createUser: (params) => axiosClient.post("api/v1/users/create-user", params)
+  createUser: (params) => axiosClient.post("api/v1/users/create-user", params),
+
+  getUsersByRestaurant: ({ limit, page, order, id, startDate, endDate, status, price, dateSort }) => {
+    const params = {
+      limit,
+      page,
+      order,
+      startDate,
+      endDate,
+      status,
+      price,
+      dateSort
+    }
+
+    const validParams = Object.fromEntries(Object.entries(params).filter(([_, value]) => value !== undefined && value))
+
+    const queryString = new URLSearchParams(validParams).toString()
+
+    const url = `api/v1/users/${id}/restaurant${queryString ? `?${queryString}` : ""}`
+
+    return axiosClient.get(url)
+  }
 }
 
 export default userApi
--- src/screens/Users/Users.jsx
+++ src/screens/Users/Users.jsx
@@ -1,50 +1,37 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import BaseLayout from "../../components/BaseLayout"
 import Button from "../../components/Button"
 import { Breadcrumbs, Text } from "@mantine/core"
 import BreadCrumbNavigation from "../../components/BreadCrumbNavigation"
 import MenuTable from "../Menu/MenuTable"
 import { useLocation, useNavigate } from "react-router-dom"
-import { useDispatch } from "react-redux"
 import { NAVIGATION_ROUTES } from "../../routes"
+import toast from "react-hot-toast"
+import userApi from "../../api/userApi"
+import { useSelector } from "react-redux"
 
 export default function Users() {
   const navigate = useNavigate()
   const location = useLocation()
-  const dispatch = useDispatch()
+  const restaurant = useSelector((state) => state.restaurant.value)
 
-  const users = [
-    {
-      id: "3f24d92a-23bc-49a7-8b94-f17f0f3e78a4",
-      name: "Geovany",
-      email: "marlon@gofood.com",
-      phoneNumber: "+504 9923 2793",
-      createdAt: new Date("2023-12-03T19:28:41.812Z"),
-      isActive: true,
-      purchases: 200,
-      image: "https://api.dicebear.com/7.x/micah/svg?backgroundColor=b6e3f4"
-    },
-    {
-      id: "3f224d92a-23bc-49a7-8b94-f17f0f3e78a4",
-      name: "GeovanyCast",
-      email: "marlon@gofood.com",
-      phoneNumber: "+504 9923 2793",
-      createdAt: new Date("2023-11-03T19:28:41.812Z"),
-      isActive: true,
-      purchases: 200,
-      image: "https://api.dicebear.com/2.x/micah/svg?backgroundColor=b6e3f4"
-    },
-    {
-      id: "3f24d92a-23bc-49a7-8b94-f17f0f3e7824",
-      name: "Castro Mejia",
-      email: "marlon@gofood.com",
-      phoneNumber: "+504 9923 2793",
-      createdAt: new Date("2023-12-03T19:21:21.832Z"),
-      isActive: true,
-      purchases: 150,
-      image: "https://placehold.co/600x400?text=Imagen+no+disponible"
+  const [users, setUser] = useState([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await userApi.getUsersByRestaurant(restaurant.id)
+        if (response.error) {
+          toast.error("Error obteniendo la información de los usuarios")
+        } else {
+          setUser(response.data)
+        }
+      } catch (error) {
+        toast.error("Fallo obtener los datos del usuario")
+      }
     }
-  ]
+    fetchUsers()
+  }, [])
 
   const refreshPage = () => {}
   const handleDisableSelected = () => {}
