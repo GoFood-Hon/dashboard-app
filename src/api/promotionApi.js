ff724ccc17122a7fcd4b4c201b4bcf28d86a4fa8
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-02-01 16:09:38 -0600

    feat: added promotion endpoints

---
+++ src/api/promotionApi.js
@@ -0,0 +1,21 @@
+import axiosClient from "./axiosClient"
+
+const promotionApi = {
+  createOffer: (params) => axiosClient.post("api/v1/offer", params),
+
+  addDishesToOffer: (params, restaurantId) => axiosClient.post(`api/v1/offer/${restaurantId}/dishes`, params),
+
+  deleteOffer: () => axiosClient.post("api/v1/offer"),
+
+  // admin-restaurant
+  getPromotionByRestaurant: () => axiosClient.get("api/v1/offer"),
+
+  // super-admin
+  getPromotionByRestaurantSuperAdmin: (restaurantId) => axiosClient.get(`api/v1/offer/${restaurantId}`),
+
+  updatePromotions: (promotionId) => axiosClient.post(`api/v1/offer/${promotionId}/images`, params),
+
+  updateStatus: (promotionId, params) => axiosClient.post(`api/v1/offer/status/${promotionId}`, params)
+}
+
+export default promotionApi
