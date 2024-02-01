28b25e8b12e5bdfb9399ea72d8eec51883cd87d9
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-02-01 16:09:27 -0600

    feat: updated couponAPI

--- src/api/couponApi.js
+++ src/api/couponApi.js
@@ -3,7 +3,9 @@ import axiosClient from "./axiosClient"
 const couponApi = {
   createCoupon: (params) => axiosClient.post("api/v1/coupon/", params),
 
-  deleteCoupon: (id, params) => axiosClient.delete(`api/v1/coupon/${id}`, params)
+  deleteCoupon: (id, params) => axiosClient.delete(`api/v1/coupon/${id}`, params),
+
+  getCoupons: () => axiosClient.get("api/v1/coupon")
 }
 
 export default couponApi
