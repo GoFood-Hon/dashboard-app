adc6248081ec872827db7bda31caaa06d739d730
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-02-05 00:23:55 -0600

    refactor: new constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,22 @@ export const APP_ROLES = {
   superAdmin: "superadmin",
   restaurantAdmin: "restaurant-admin"
 }
+
+export const userTypes = [
+  {
+    value: "kitchen",
+    label: "Cocina"
+  },
+  {
+    value: "cashier",
+    label: "Cajero"
+  },
+  {
+    value: "driver",
+    label: "Driver"
+  },
+  {
+    value: "admin",
+    label: "Administrador"
+  }
+]
