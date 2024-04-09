80a128b47b2877316d903cc009b0f53d6a8f23cd
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-04-08 23:22:19 -0600

    feat: added api for offers

--- src/api/couponApi.js
+++ src/api/couponApi.js
@@ -1,11 +1,17 @@
 import axiosClient from "./axiosClient"
 
 const couponApi = {
+  // Create coupon
   createCoupon: (params) => axiosClient.post("api/v1/coupon/", params),
-
+  // Delete coupon
   deleteCoupon: (id, params) => axiosClient.delete(`api/v1/coupon/${id}`, params),
-
-  getCoupons: () => axiosClient.get("api/v1/coupon")
+  // Get coupon history
+  getCoupons: () => axiosClient.get("api/v1/coupon"),
+  // Add or update image
+  addImage: (couponId, params) =>
+    axiosClient.post(`api/v1/coupon/${couponId}/images`, params, {
+      contentType: `multipart/form-data; boundary=${params._boundary}`
+    })
 }
 
 export default couponApi
--- src/api/promotionApi.js
+++ src/api/promotionApi.js
@@ -1,20 +1,27 @@
 import axiosClient from "./axiosClient"
 
 const promotionApi = {
+  // Create Offer
   createOffer: (params) => axiosClient.post("api/v1/offer", params),
 
+  // Add dishes to offer
   addDishesToOffer: (params, promotionId) => axiosClient.post(`api/v1/offer/${promotionId}/dishes`, params),
 
+  // Delete offer
   deleteOffer: () => axiosClient.post("api/v1/offer"),
 
-  // admin-restaurant
+  // Get offer by restaurante -> admin-restaurant
   getPromotionByRestaurant: () => axiosClient.get("api/v1/offer"),
 
-  // super-admin
+  // Get offer by restaurant -> super-admin
   getPromotionByRestaurantSuperAdmin: (restaurantId) => axiosClient.get(`api/v1/offer/${restaurantId}`),
 
-  updatePromotions: (promotionId) => axiosClient.post(`api/v1/offer/${promotionId}/images`, params),
-
+  // Update or Add offer images
+  addImage: (promotionId, params) =>
+    axiosClient.post(`api/v1/offer/${promotionId}/images`, params, {
+      contentType: `multipart/form-data; boundary=${params._boundary}`
+    }),
+  // Update offer status
   updateStatus: (promotionId, params) => axiosClient.post(`api/v1/offer/status/${promotionId}`, params)
 }
 
