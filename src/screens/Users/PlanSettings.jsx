b73db935d606ecca0722d8f370bd3680a58f94ad
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-04-22 15:41:42 -0600

    feat: show plan

--- src/screens/Users/PlanSettings.jsx
+++ src/screens/Users/PlanSettings.jsx
@@ -2,50 +2,61 @@ import React from "react"
 import BaseLayout from "../../components/BaseLayout"
 import { Breadcrumbs, Grid, Tabs } from "@mantine/core"
 import BreadCrumbNavigation from "../../components/BreadCrumbNavigation"
-import { useForm } from "react-hook-form"
 import SettingsCard from "../../components/SettingsCard"
+import { useSelector } from "react-redux"
 
 export default function PlanSettings() {
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors }
-  } = useForm({})
-
-  const onSubmit = async (data) => {}
+  const user = useSelector((state) => state.user.value)
+  const planData = user.Restaurant.Subscription.Plan
 
   return (
     <BaseLayout>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="pl-[200px]">
-          <section>
-            <div className="flex flex-row justify-between items-center pb-6">
-              <div className="flex flex-row gap-x-3 items-center">
-                <h1 className="text-white-200 text-2xl font-semibold">Plan</h1>
-              </div>
-              <div>
-                <Breadcrumbs>
-                  <BreadCrumbNavigation location={location} />
-                </Breadcrumbs>
-              </div>
+      <div className="pl-[200px]">
+        <section>
+          <div className="flex flex-row justify-between items-center pb-6">
+            <div className="flex flex-row gap-x-3 items-center">
+              <h1 className="text-white-200 text-2xl font-semibold">Plan</h1>
             </div>
-          </section>
-          <Tabs defaultValue="payments" color="#0e2946">
-            <Tabs.List>
-              <Tabs.Tab value="payments">Pagos</Tabs.Tab>
-              <Tabs.Tab value="history">Historial de pagos</Tabs.Tab>
-            </Tabs.List>
-            <Tabs.Panel value="payments">
-              <SettingsCard title="Plan" iconName="creditCard"></SettingsCard>
-              <SettingsCard title="Pago" iconName="creditCard"></SettingsCard>
-            </Tabs.Panel>
-            <Tabs.Panel value="history">
-              <SettingsCard title="Historial de pagos" iconName="creditCard"></SettingsCard>
-            </Tabs.Panel>
-          </Tabs>
-        </div>
-      </form>
+            <div>
+              <Breadcrumbs>
+                <BreadCrumbNavigation location={location} />
+              </Breadcrumbs>
+            </div>
+          </div>
+        </section>
+        <Tabs defaultValue="payments" color="#0e2946">
+          <Tabs.List>
+            <Tabs.Tab value="payments">Plan Activo</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel value="payments">
+            <SettingsCard title={`Plan: ${planData.name}`} iconName="creditCard">
+              <PlanDetailsCard plan={planData} />
+            </SettingsCard>
+          </Tabs.Panel>
+        </Tabs>
+      </div>
     </BaseLayout>
+  )
+}
+
+const PlanDetailsCard = ({ plan }) => {
+  const { name, price, tax, currency, paymentType, PlanFeatures } = plan
+
+  return (
+    <div className="border rounded-lg border-blue-100 p-4 bg-white m-4">
+      <h1 className="text-lg font-semibold mb-2">{name}</h1>
+      <p>
+        <span className="font-semibold">Price:</span> {currency} {price} per {paymentType.toLowerCase()} (including {tax}% tax)
+      </p>
+      <h2 className="text-md font-semibold mt-4 mb-2">Features Included:</h2>
+      <ul className="list-disc pl-6">
+        {PlanFeatures.map((feature) => (
+          <li key={feature.id}>
+            {feature.name}: {feature.PlanPlanFeatures.quan}{" "}
+            {feature.type === "amount" ? "" : feature.PlanPlanFeatures.avai ? "Included" : "Not Included"}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
