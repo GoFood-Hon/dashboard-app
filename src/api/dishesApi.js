a1920e0230fcb5f6b884cb022eea359f76b0966d
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-05-10 11:03:48 -0600

    Revert "new endpoint created in dishes API"
    This reverts commit 942f2a5489ecc601db1de5f0e7b48f916a34087a.

--- src/api/dishesApi.js
+++ src/api/dishesApi.js
@@ -25,7 +25,7 @@ const dishesApi = {
 
   getDish: (dishId) => axiosClient.get(`api/v1/dish/${dishId}`),
 
-  createDish: (formData) => axiosClient.post("api/v1/dish/create/with-extras", formData),
+  createDish: (formData) => axiosClient.post("api/v1/dish/", formData),
 
   createDishWithExtra: (formData) => axiosClient.post("api/v1/dish/create/with-extras", formData),
 
